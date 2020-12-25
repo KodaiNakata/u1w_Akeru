4187f902b3f30fb6a1221c1fd9201e83ee2d535a
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2020-12-25 09:56:06 +0900

    ファイル名の変更

--- Akeru/Assets/Script/Common/ClosePanelButton.cs
+++ Akeru/Assets/Script/Common/ClosePanelButton.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 /**
-* @file CloseButton.cs
-* @brief 閉じるボタンを操作するファイル
+* @file ClosePanelButton.cs
+* @brief パネルを閉じるボタンを操作するファイル
 * @author Kodai Nakata
 */
 
 /**
- * @class CloseButton
- * @brief 閉じるのボタン用のクラス
+ * @class ClosePanelButton
+ * @brief パネルを閉じるのボタン用のクラス
  */
-public class CloseButton : MonoBehaviour
+public class ClosePanelButton : MonoBehaviour
 {
     /**
      * @brief クリック通知
---
+++ Akeru/Assets/Script/Common/OpenPanelButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/**
+* @file OpenPanelButton.cs
+* @brief パネルを開くボタンを操作するファイル
+* @author Kodai Nakata
+*/
+
+/**
+ * @class OpenPanelButton
+ * @brief パネルを開くボタン用のクラス
+ */
+public class OpenPanelButton : MonoBehaviour
+{
+    //! パネルを開くためのオブジェクト
+    private GameObject openPanelObj;
+
+    /**
+     * @brief 最初のフレームに入る前に呼び出される関数
+     */
+    void Start()
+    {
+        openPanelObj = GameObject.FindGameObjectWithTag("PauseObject");
+    }
+
+    /**
+     * @brief クリック通知
+     */
+    public void OnClick()
+    {
+        openPanelObj.transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
